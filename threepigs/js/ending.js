fdaf9e8bc72f7478020109bc00d3dd7e4fd1c10b
Sarah Danseglio <42783627+dansegliode@users.noreply.github.com>
2018-12-03 11:20:45 -0500

    added interaction project

---
+++ threepigs/js/ending.js
@@ -0,0 +1,23 @@
+//fade animations
+document.addEventListener("DOMContentLoaded", function (event) {
+	// elements with fade are equipt with the fade class name
+    document.getElementById('fade1').className += ' fade';
+    document.getElementById('fade6').className += ' fade';
+    $('.storyPara').addClass('storyFadeOut');
+});
+// classes are removed after transition
+$(function () {
+    $('#fade1').removeClass('fade');
+    $('#fade6').removeClass('fade');
+});
+// function used at the end of a scene to transition out
+setTimeout(function () {
+    document.addEventListener("wheel", function (e) {
+        $('.storyPara').removeClass('storyFadeOut');
+        e.preventDefault();
+
+        $(".sceneIntroContainer").fadeOut(500, function () {
+            window.location = "index.html";
+        });
+    });
+}, 2700);
